3657e9c94ca4e621211c720695c38d2376f95cbc
DemonDED <82447153+DemonDED@users.noreply.github.com>
2022-04-21 23:48:19 +0700

    готовы_запросы_регистрации_и_авторизации

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,31 @@
-const xhr = new XMLHttpRequest();
 const userName = document.getElementById('userName');
 const password = document.getElementById('password');
+const contact = document.querySelector('#contact');
 const email = document.getElementById('email');
 const addres = document.getElementById('addres');
 const tin = document.getElementById('tin');
+const login = document.querySelector('#login');
+const sendData = document.querySelector('#sendData');
 
-let data = `{"userName": "${FIO.value}", "password": "${password.value}", "email": "${email.value}", "addres": "${addres.value}", "tin": "${tin.value}"}`;
+async function createUser() {
 
-xhr.open('POST', '', true);
-xhr.onload = () => {
-    if (xhr.status == 200) {
-
+    let createUserData = `{"userName": "${userName.value}", "password": "${password.value}", "email": "${email.value}","accessLevel": "1", "address": "${addres.value}", "contact": "${contact.value}", "tin": "${tin.value}"}`;
+    let response = await fetch('http://90.189.211.207/createUser', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json;charset=utf-8'},
+        body: createUserData
+    });
+    let result = await response.json();
+    if (result) {
+        window.location.href = '/userCabinet.html';
+    } else {
+        alert('Ошибка при создании пользователя');
     }
 }
-xhr.send(data);
+
+function loginEvent() {
+    window.location.href = '/login.html';
+}
+
+sendData.addEventListener('click', createUser);
+login.addEventListener('click', loginEvent);
--- frontend/userData.js
+++ frontend/userData.js
@@ -1,9 +1,33 @@
-const xhr = new XMLHttpRequest();
+const test = document.querySelector('.testText');
 
-xhr.open('GET', 'http://90.189.211.207/getUserInformation/Roman', true);
-xhr.onload = () => {
+test.textContent = 'dasdsads'
+
+async function getUserInformation() {
+
+  xhr.open('GET', 'http://90.189.211.207/getUserInformation/Roman', true);
+  xhr.onload = () => {
     if (xhr.status == 200) {
-
+        let userData = xhr.response;
     }
+  }
+  xhr.send();
+
 }
-xhr.send();
+
+function getTaxReporting() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
