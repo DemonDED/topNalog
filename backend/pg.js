5e25c6cf8d8ae6fd0562ff9261e100dc9e50d239
Roman Tokmin <roman_tokmin@mail.ru>
2022-04-22 00:29:02 +0700

    Login endpoint

--- backend/pg.js
+++ backend/pg.js
@@ -7,17 +7,7 @@ const client = new Client({
 
 client.connect();
 
-async function getUserInformation(userName) {
-    const select = 'SELECT * FROM users WHERE name=$1';
-    try {
-        return (await client.query(select, [userName])).rows[0];
-    } catch(err) {
-        return err.stack;
-    }
-}
-
-async function createUser(data) {
-    console.log(data);
+async function createUser(data, response) {
     const select = `SELECT * FROM users WHERE name=$1`;
     const insert = `INSERT INTO users(name, password, email, access_level, address, contact, tin)
                     VALUES ($1, $2, $3, $4, $5, $6, $7);`;
@@ -27,16 +17,46 @@ async function createUser(data) {
         const selectResult = await client.query(select, [data.userName]);
 
         if (selectResult.rows.length > 0) {
-            return 'User with this name already exist';
+            response.status(400).send(JSON.stringify({"result": "User already exist"}));
         } else {
-            client.query(insert, values);
-
-            return `Created user with name ${data.userName}`;
+            const result = await client.query(insert, values);
+            if (result.rowCount === 1) {
+                response.status(200).send(JSON.stringify({"result": "User created"}));
+            } else {
+                response.status(200).send(JSON.stringify({"result": "Не создался пользователь. Почему? Я хуй знает"}));
+            }
+            
         }
     } catch(err) {
-        return err.stack;
+        response.status(500).send(JSON.stringify({"result": "На сервере какая-то ошибка, я хуй знает"}));
     }
 }
 
+async function login(data, response) {
+    const select = `SELECT password FROM users WHERE name=$1;`;
+    try {
+        const result = await (await client.query(select, [data.userName])).rows[0];
+        if(result.password === data.password) {
+            response.status(200).send(JSON.stringify({"result": "Авторизация успешна"}));
+        } else {
+            response.status(400).send(JSON.stringify({"result": "Неверный пароль"}));
+        }
+    } catch(error) {
+        response.status(500).send(JSON.stringify({"result": "На сервере какая-то ошибка, я хуй знает"}));
+    }
+}
+
+async function getUserInformation(userName, response) {
+    const select = 'SELECT * FROM users WHERE name=$1';
+    try {
+        const result = await client.query(select, [userName]).rows[0];
+        response.status(200).send(JSON.stringify(result));
+    } catch(err) {
+        response.status(500).send(JSON.stringify({"result": "На сервере какая-то ошибка, я хуй знает"}));
+    }
+}
+
+
 exports.getUserInformation = getUserInformation;
 exports.createUser = createUser;
+exports.login = login;
